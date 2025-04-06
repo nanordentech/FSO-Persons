1592b30f2c9afc68fb06d2c448a2a31eadbba5e7
Nathan Norden <nnorden@Mac.fios-router.home>
2025-04-06 12:38:29 -0400

    uibuild

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,7 +6,6 @@ export default [
   js.configs.recommended,
   {
     files: ['**/*.js'],
-    ignores: ['dist/**', 'test.js'],
     languageOptions: {
       sourceType: 'commonjs',
       globals: { ...globals.node },
@@ -20,7 +19,15 @@ export default [
       '@stylistic/js/linebreak-style': ['error', 'unix'],
       '@stylistic/js/quotes': ['error', 'single'],
       '@stylistic/js/semi': ['error', 'never'],
+      eqeqeq: 'error',
+      'no-trailing-spaces': 'error',
+      'object-curly-spacing': ['error', 'always'],
+      'arrow-spacing': ['error', { before: true, after: true }],
+      'no-console': 'off',
     },
   },
+  {
+    ignores: ['dist/**', 'test.js'],
+  }
 
 ]
--- mongo.js
+++ mongo.js
@@ -35,7 +35,7 @@ else if (process.argv.length === 5) {
         number: process.argv[4],
     })
 
-    person.save().then(result => {
+    person.save().then(() => {
         console.log('person saved!')
         mongoose.connection.close()
     })
